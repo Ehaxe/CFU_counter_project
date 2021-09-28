2134acc2ba3958b3290b0c7ace78c107ff387cac
Ehaxe <90378339+Ehaxe@users.noreply.github.com>
2021-09-28 18:07:08 +0200

    modified file inclusions

--- CFU_Counter_project/CFU_Counter_functions.py
+++ CFU_Counter_project/CFU_Counter_functions.py
@@ -4,3 +4,12 @@ import cv2 as cv
 import numpy as np
 #import imutils as im
 
+# getting arguement and parsing it
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--image", required=True, help="path to image")
+parser.add_argument("-o", "--output", required=True, help="path to output")
+args = vars(parser.parse_args())
+
+# loading of image
+image_orig =cv.imread(args["image"])
+height_orig, width.orig = image_orig.shape[:2]
