7d4191b382c0400bc31ee102ecfabe11df8c3211
Ehaxe <90378339+Ehaxe@users.noreply.github.com>
2021-10-11 16:08:32 +0200

    added functions to photo management

--- CFU_Counter_project/CFU_Counter_main.py
+++ CFU_Counter_project/CFU_Counter_main.py
@@ -3,14 +3,14 @@ import tkinter as tk
 import webbrowser
 import cv2
 import numpy as np
-#import Image, ImageTk
+import PIL.Image, PIL.ImageTk
 from tkinter import filedialog
 
 # initializing main loop
 root = tk.Tk()
 root.title("CFU Counter")
 root.geometry("1200x1200")
-root.resizable(width=False, height=False)
+#root.resizable(width=False, height=False)
 
 
 
@@ -31,33 +31,48 @@ class menuBar(tk.Frame):
     def menu_widget(self):
 
         topBar = tk.Menu(root)
+
+        # first drop-dwon menu
         firstMenu = tk.Menu(topBar, tearoff=0)
         firstMenu.add_command(label="Open image:", command=openCV.open_image)
         firstMenu.add_command(label="Exit", command=root.quit)
         topBar.add_cascade(label="File", menu=firstMenu)
-
+        # second drop-dwon menu
         secondMenu = tk.Menu(topBar, tearoff=0)
         secondMenu.add_command(label="Set area (rectangle):", command=root.quit)
         secondMenu.add_command(label="Set area (polygon):", command=root.quit)
         secondMenu.add_command(label="Count Colonies:", command=root.quit)
         topBar.add_cascade(label="Processing", menu=secondMenu)
-
+        # third drop-dwon menu
         thirdMenu = tk.Menu(topBar, tearoff=0)
         thirdMenu.add_command(label="ReadMe Online", command=self.openURL)
         topBar.add_cascade(label="Help", menu=thirdMenu)
-    
+        # forming menues
         root.config(menu=topBar)
 
 
-# class for loading and manage images
+# class for loading and managing images
 class openCV():
     # importing important libraries
     
 
 
     def open_image():
-        filedialog.askopenfile(initialdir = "/",title = "Select image",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+        image_orig = cv2.imread(filedialog.askopenfilename(initialdir = "/",title = "Select image",filetypes = (("jpeg files","*.jpg"),("all files","*.*"))))
+        if len(image_orig) > 0:
+            print ("yes")
+        print ("image loaded")   
+        image_gray = cv2.cvtColor(image_orig, cv2.COLOR_BGR2GRAY)
+        
+        #store image as variable and get dimensions
+        height = image_gray.shape[0]
+        width = image_gray.shape[1]
+        print (height, width)
+        
+        image_canvas = tk.Canvas(root, width = width, height = height)
+        image_canvas.grid()
 
+        cv2.imshow(image_canvas, image_gray)
 
     def draw_polygon():
         pass
