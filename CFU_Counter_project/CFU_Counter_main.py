d576437804dc8248d5546fefd3cd30af19b534fc
Ehaxe <90378339+Ehaxe@users.noreply.github.com>
2021-10-11 09:12:46 +0200

    Pushed from mained

--- CFU_Counter_project/CFU_Counter_main.py
+++ CFU_Counter_project/CFU_Counter_main.py
@@ -1,13 +1,65 @@
 # importing critical libraries
-import CFU_GUI_class
-from tkinter import *
+import tkinter as tk
+import webbrowser
+import cv2
+import numpy as np
+import Image, ImageTk
 
-object = CFU_GUI_class.CFU_GUI
 
 # initializing main loop
-root = tkinter.Tk()
+root = tk.Tk()
 root.title("CFU Counter")
-root.geometry("800x1200")
+root.geometry("1200x1200")
 root.resizable(width=False, height=False)
-app = object(root)
+
+
+
+# setting up class for defining a GUI frame
+class menuBar(tk.Frame):
+    
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.menu_widget()
+
+    def openURL(self):
+        url = "https://github.com/Ehaxe/CFU_counter_project/blob/main/README.md"
+        new=1
+        webbrowser.open(url,new=new)
+
+        # creating the different menu widgets
+    def menu_widget(self):
+
+        topBar = tk.Menu(root)
+        firstMenu = tk.Menu(topBar, tearoff=0)
+        firstMenu.add_command(label="Open file:", command=root.quit)
+        firstMenu.add_command(label="Exit", command=root.quit)
+        topBar.add_cascade(label="File", menu=firstMenu)
+
+        secondMenu = tk.Menu(topBar, tearoff=0)
+        secondMenu.add_command(label="Set area (rectangle):", command=root.quit)
+        secondMenu.add_command(label="Set area (polygon):", command=root.quit)
+        secondMenu.add_command(label="Count Colonies:", command=root.quit)
+        topBar.add_cascade(label="Processing", menu=secondMenu)
+
+        thirdMenu = tk.Menu(topBar, tearoff=0)
+        thirdMenu.add_command(label="ReadMe Online", command=self.openURL)
+        topBar.add_cascade(label="Help", menu=thirdMenu)
+    
+        root.config(menu=topBar)
+
+
+# class for loading and manage images
+class openCV():
+    # importing important libraries
+    
+
+
+    def open_image():
+        askopenfile(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+
+
+    def 
+    
+app = menuBar(root)
 root.mainloop()
--- CFU_Counter_project/CFU_GUI_class.py
+++ CFU_Counter_project/CFU_GUI_class.py
@@ -1,7 +1,7 @@
 # importing of necessary libraries
 from tkinter import *
 
-# setting up class for defining a GUI frame
+"""# setting up class for defining a GUI frame
 class CFU_GUI(Frame):
     # initializing class
     def __init__(self, master):
@@ -10,13 +10,17 @@ class CFU_GUI(Frame):
         self.grid()
         self.create_widgets()
 
-    def create_widgets(self):
         # creating the actual widgets from tkinter
+    def create_widgets(self):
+        
         menubar = Menu(root)
         file_menu = Menu(menubar, tearoff=0)
         file_menu.add_command(label="Select image", command=root.quit)
         file_menu.add_command(label="Exit", command=root.quit)
         file_menu.add_cascade(label="File", menu=file_menu)
+
+    def open_File():
+        askopenfile(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))"""
 
 
 
