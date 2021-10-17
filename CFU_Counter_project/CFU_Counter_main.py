fe6a226bb2531cf59c8cdfe78bbb34fe6536dc2d
Ehaxe <90378339+Ehaxe@users.noreply.github.com>
2021-10-17 20:09:02 +0200

    consolidated project

--- CFU_Counter_project/CFU_Counter_main.py
+++ CFU_Counter_project/CFU_Counter_main.py
@@ -6,21 +6,19 @@ import numpy as np
 import PIL.Image, PIL.ImageTk
 from tkinter import filedialog
 
-
 # initializing main loop
 root = tk.Tk()
 root.title("CFU Counter")
-root.geometry("250x0")
 root.resizable(width=False, height=False)
 
-# setting up class for defining a GUI frame
+# setting up class for defining a GUI frame, self initialises
 class menuBar(tk.Frame):
     
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         self.grid()
         self.menu_widget()
-
+    # open readme file online
     def openURL(self):
         url = "https://github.com/Ehaxe/CFU_counter_project/blob/main/README.md"
         new=1
@@ -28,9 +26,8 @@ class menuBar(tk.Frame):
 
         # creating the different menu widgets
     def menu_widget(self):
-
+        # defining topbar menu
         topBar = tk.Menu(root)
-
         # first drop-dwon menu
         firstMenu = tk.Menu(topBar, tearoff=0)
         firstMenu.add_command(label="Open image:", command=openCV.open_image)
@@ -49,26 +46,40 @@ class menuBar(tk.Frame):
         # forming menues
         root.config(menu=topBar)
 
+        #using filedialog to open path dialog and passing it to c2.imread function
+        start_img = cv2.imread("start_img.jpg")
+        # store image as variable and get dimensions
+        height = start_img.shape[0]
+        width = start_img.shape[1]
+        # dynamically set root window dimensions
+        geometry = str(width)+"x"+str(height)
+        root.geometry(geometry)
+
+        # define TK_img as global variable to prevent garbage collection
+        global TK_img
+        global canvas
+        global img_canvas
+        # convert to Pillow      
+        Pil_img_start = PIL.Image.fromarray(start_img)        
+        TK_img_start = PIL.ImageTk.PhotoImage(image=Pil_img_start)        
+        # create canvas to frame image and place image within canvas
+        canvas = tk.Canvas(root, width = width, height = height)
+        canvas.grid()
+        img_canvas = canvas.create_image(0,0, image=TK_img_start, anchor=tk.NW)
 
 # class for loading and managing images
 class openCV():
     
     def open_image():
         #using filedialog to open path dialog and passing it to c2.imread function
-        img_orig = cv2.imread(filedialog.askopenfilename(initialdir = "/",title = "Select image",filetypes = (("jpeg files","*.jpg"),("all files","*.*"))))
-        if len(img_orig) > 0:
-            print ("yes")
-            print ("image loaded")
+        img_orig = cv2.imread(filedialog.askopenfilename(initialdir = "/",title = "Select image",filetypes = (("jpeg files","*.jpg"),("all files","*.*"))))        
         #convert and store loaded image as grayscale image and make it global
         global img_gray
         img_gray = cv2.cvtColor(img_orig, cv2.COLOR_BGR2GRAY)
-        
-        
 
         # store image as variable and get dimensions
         height = img_gray.shape[0]
         width = img_gray.shape[1]
-        print (height, width)
         # add extra spacing around edge
         gheight = height+2
         gwidth = width+2
@@ -78,16 +89,15 @@ class openCV():
 
         # define TK_img as global variable to prevent garbage collection
         global TK_img
+        global canvas
         global img_canvas
         # convert to Pillow      
         Pil_img = PIL.Image.fromarray(img_gray)        
         TK_img = PIL.ImageTk.PhotoImage(image=Pil_img)        
         # create canvas to frame image and place image within canvas
-        img_canvas = tk.Canvas(root, width = width, height = height)              
-        img_canvas.create_image(0,0, image=TK_img, anchor=tk.NW)        
-        img_canvas.grid()
-        
-     
+        canvas.itemconfigure(img_canvas,image=TK_img)
+        canvas.config(height = gheight, width = gwidth)
+
     def count_CFU():
 
         # setting of parameters for blob detection
@@ -112,18 +122,16 @@ class openCV():
         keypoints = detector.detect(img_gray)
         img_with_keypoints = cv2.drawKeypoints(img_gray, keypoints, np.array([]), (0,0,255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         
-        # opencv image show to compare to pillow
-        cv2.imshow("image_canvas", img_with_keypoints)
-        print (len(keypoints))        
-        Pil_circle_img = PIL.Image.fromarray(img_with_keypoints)        
-        TK_circle_img = PIL.ImageTk.PhotoImage(image=Pil_circle_img)
-        #img_canvas.itemconfigure(img_canvas,image=TK_circle_img)
+        # read in image with circles and swap out image to canvas
+        Pil_circle_img = PIL.Image.fromarray(img_with_keypoints)
+        global TK_circle_img
+        TK_circle_img = PIL.ImageTk.PhotoImage(image=Pil_circle_img)        
+        canvas.itemconfigure(img_canvas,image=TK_circle_img)
 
         # create and display CFU count values
         colony_count = "Number of colonies: "+str(len(keypoints))
-        print (colony_count)
-        tk.messagebox.showinfo(title="Colony count", message=colony_count)
-        
+        tk.messagebox.showinfo(title="Colony count", message=colony_count)        
     
 app = menuBar(root)
 root.mainloop()
+# end of mainloop
